1b4624508e7944a73d1dce8a187c27a80a812eeb
Patrik Wehovszky <90713831+wehopatrik@users.noreply.github.com>
2022-01-13 12:15:45 +0100

    Delete select_all_tables.sql

--- f1_manager/select_all_tables.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM motorgyarto ORDER BY motorgyarto_id;
-SELECT * FROM csapat ORDER BY csapat_id;
-SELECT * FROM pilota_osszes ORDER BY szemely_id;
-SELECT * FROM pilota_csapat ORDER BY pilota_csapat_id;
-SELECT * FROM futam_osszes ORDER BY futam_id;
-SELECT * FROM futam_ev ORDER BY futam_ev_id;
