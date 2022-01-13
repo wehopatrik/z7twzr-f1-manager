866fe164dc1767ad08e582363c318603bee22c46
Patrik Wehovszky <90713831+wehopatrik@users.noreply.github.com>
2022-01-13 12:15:50 +0100

    Delete select_all_history_tables.sql

--- f1_manager/select_all_history_tables.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM motorgyarto_h ORDER BY modositva DESC;
-SELECT * FROM csapat_h ORDER BY modositva DESC;
-SELECT * FROM pilota_osszes_h ORDER BY modositva DESC;
-SELECT * FROM pilota_csapat_h ORDER BY modositva DESC;
-SELECT * FROM futam_osszes_h ORDER BY modositva DESC;
-SELECT * FROM futam_ev_h ORDER BY modositva DESC;
